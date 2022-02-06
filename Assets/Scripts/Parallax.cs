65df0242d60a7bbc82b03bb52bff7f645c584a3e
TimAlt <metaltimmay@gmail.com>
2022-02-05 22:34:29 -0500

    Parallax background and camera update

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public GameObject Camera;
+    public float parallaxMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 distance = new Vector2(Camera.transform.position.x, Camera.transform.position.y) * parallaxMultiplier;
+        transform.position = distance;
+    }
+}
